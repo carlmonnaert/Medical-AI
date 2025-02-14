f9e3f38bf0debbd0a93fe1fbf5c58b596d9cf5ae
carlmonnaert <monnaert.carl@gmail.com>
2025-02-14 10:12:17 +0100

    Magnifique fichier de classes

--- class.py
+++ class.py
@@ -1,5 +1,5 @@
 class Medecin:
-    
+
     def __init__(self):
         self.patients = []
         self.en_charge = None
@@ -9,11 +9,25 @@ class Medecin:
 
 class Patient:
 
-    def __init__(self, arrival_time):
-
+    def __init__(self, arrival_time=0):
+        self.arrival_time = arrival_time
 
 class Hopital:
 
-    def __init__(self, beds = 500):
+    def __init__(self, beds = 500, medecins = []):
         self.beds = beds
-        self.file = 
+        self.file_attente = []
+        self.medecins = medecins
+
+class Maladie:
+    def __init__(sefl,taux_mortalite, taux_contagion,temps_rémission):
+        self.taux_mortalite = taux_mortalite
+        self.taux_contagion = taux_contagion
+        self.temps_rémission = temps_rémission
+
+class World:
+    # Monde à temps discret par jours
+    def __init__(self, hopitaux = [], population = 1000):
+        self.hopitaux = hopitaux
+        self.population = population
+        self.maladies = []
